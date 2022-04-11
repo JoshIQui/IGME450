513fbdb65143ba82b2a95d2b563b759c8d471e40
jiq4449 <jiq4449@IGM2435-27>
2022-04-11 13:55:47 -0400

    Fixed UI anchors

--- Assets/Scripts/StarCounter.cs
+++ Assets/Scripts/StarCounter.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StarCounter : MonoBehaviour
 {
     //[SerializeField] DEBUG SERIALIZATION
     public int starCount = 0;
 
-    private GameObject[] starHoles = new GameObject[3];
+    [SerializeField]
+    private Image[] starHoles = new Image[3];
 
     private void Start()
     {
         for (int i = 0; i < 3; i++)
         {
-            starHoles[i] = GameObject.Find("StarHole" + (i + 1));
+            //starHoles[i] = GameObject.Find("StarHole" + (i + 1));
             //if (starCount > i) starHoles[i].SetActive(false); DEBUG INITIALIZE STARS
         }
     }
@@ -21,15 +23,15 @@ public class StarCounter : MonoBehaviour
     public void IncreaseStarCount()
     {
         print("Star " + starCount + " Filled");
-        starHoles[starCount].SetActive(false);
+        //starHoles[starCount].SetActive(false);
         starCount++;
     }
 
     public void ResetStarCount()
     {
-        foreach (GameObject star in starHoles)
+        foreach (Image star in starHoles)
         {
-            star.SetActive(true);
+            star.color = Color.gray;
         }
         starCount = 0;
     }
